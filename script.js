95e074a8e17f2ef969aec67becc442343df336e2
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-21 19:21:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+var k=document.getElementById("timer");
+let current=new Date();
+setInterval(
+	function(){
+		current=new Date();
+		k.innerHTML=current.toLocaleString();
+	},1000
+)
+
+
